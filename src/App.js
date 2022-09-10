2cf60588d569526bbf50301d0416a3e4c241c909
Parker Parham <parkerparham@gmail.com>
2022-09-10 11:03:30 -0400

    commit tree 87d23496867e15ab885daebaf98e971642eb5596

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [images, setImages] = useState([]);
+
+  useEffect(
+    // 1st arg: side effect (callback function)
+    () => console.log("useEffect called"),
+    // 2nd arg: dependencies array
+    [count]
+  );
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((r) => r.json())
+      .then((data) => {
+        setImages(data.message);
+      });
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    <div>
+      {images.map((image) => (
+        <img src={image} key={image} />
+      ))}
+    </div>
+    </div>
+  );
 }
-
 export default App;
